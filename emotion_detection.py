aefd9de0be572a8f03ef02d3964d0fb1ff958636
Valenti1967 <valenti.vlaev@gmail.com>
2025-03-19 16:02:44 -0400

    Task 3 finished

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,4 @@
-
+import json
 import requests
 
 def emotion_detector(text_to_analyse):
@@ -14,5 +14,28 @@ def emotion_detector(text_to_analyse):
     # Sending a POST request to the emotion predict API
     response = requests.post(url, json=myobj, headers=header, timeout=10)
 
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+
+    # Extract the scores from the JSON response
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+
+    # Map the emotion scores to their respective emotions
+    predicted_emotion_scores = {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score
+    }
+
+    # Find the emotion with the maximum score
+    dominant_emotion = max(predicted_emotion_scores, key=predicted_emotion_scores.get)
+
+    # Add the dominant emotion to the dictionary
+    predicted_emotion_scores['dominant_emotion'] = dominant_emotion
+
     # Returning a response as text
-    return response.text
+    return predicted_emotion_scores
